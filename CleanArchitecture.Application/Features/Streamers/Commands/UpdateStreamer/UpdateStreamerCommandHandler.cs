c1e7efdf960c0c5ec549e0bf28488569f188150e
Kevin Camargo Bautista <cmargokk@hotmail.com>
2023-10-16 22:35:12 -0500

    Delete streamer ocmmand and updatestreamercommand name correction

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/UpdateStreamer/UpdateStreamerCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Exceptions;
+using CleanArchitecture.Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.UpdateStreamer
+{
+    public class UpdateStreamerCommandHandler : IRequestHandler<UpdateStreamerCommand, Unit>
+    {
+        private readonly IStreamerRepository _streamerRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateStreamerCommandHandler> _logger;
+
+        public UpdateStreamerCommandHandler(
+            IStreamerRepository streamerRepository, 
+            IMapper mapper, 
+            ILogger<UpdateStreamerCommandHandler> logger)
+        {
+            _streamerRepository = streamerRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+        public async Task<Unit> Handle(UpdateStreamerCommand request, CancellationToken cancellationToken)
+        {
+            var streamerToUpdate = await _streamerRepository.GetByIdAsync(request.Id);
+
+            if (streamerToUpdate == null)
+            {
+                _logger.LogError($"No se encontro el streamer con el id {request.Id}");
+                throw new NotFoundException(nameof(Streamer), request.Id);
+            }
+
+            _mapper.Map(request, streamerToUpdate,typeof(UpdateStreamerCommand),typeof(Streamer));
+
+            await _streamerRepository.UpdateAsync(streamerToUpdate);
+
+            _logger.LogInformation($"La operacion fue exitosa actualizando el streamer con el id {request.Id}");
+
+            return Unit.Value;
+
+        }
+    }
+}
