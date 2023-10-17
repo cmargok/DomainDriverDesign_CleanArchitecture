ac7b1cbd44987b06294aaa74a239ea8f9b3ffff4
Kevin Camargo Bautista <cmargokk@hotmail.com>
2023-10-16 22:45:21 -0500

    ValidationException to catch validations failerus frmro fluentvalidation

--- CleanArchitecture.Application/Exceptions/NotFoundException.cs
+++ CleanArchitecture.Application/Exceptions/NotFoundException.cs
@@ -1,16 +1,16 @@
-﻿
-namespace CleanArchitecture.Application.Exceptions
+﻿namespace CleanArchitecture.Application.Exceptions
 {
     public class NotFoundException : ApplicationException
     {
+        public NotFoundException()
+        {
+
+        }
         public NotFoundException(string name, object key) 
             : base($"Entity \"{name}\" ({key}) no fue encontrado ")
         {
             
         }
-        public NotFoundException()
-        {
-            
-        }
+       
     }
 }
