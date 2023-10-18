911f4cc169d5ba377c7b230ef6f38343fc5a8c79
Kevin Camargo Bautista <cmargokk@hotmail.com>
2023-10-17 21:17:28 -0500

    savechangesasync override Dependency injecto for dbcontext

---
+++ CleanArchitecture.Data/Persistence/StreamerDbContext.cs
@@ -0,0 +1,76 @@
+﻿using CleanArchitecture.Domain;
+using CleanArchitecture.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContext : DbContext
+    {
+        /*   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+           {
+               optionsBuilder
+                   .UseSqlServer(@"Server=DESKTOP-3VP26G1\CMARGOKCLU1; Database=StreamerDB; Persist Security Info=True; Trusted_Connection=True; MultipleActiveResultSets=True; TrustServerCertificate=true;")
+               .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+               .EnableSensitiveDataLogging();
+           }
+        */
+
+        public StreamerDbContext(DbContextOptions<StreamerDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch(entry.State) 
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "system";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                        entry.Entity.LastModifiedBy = "system";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+                .HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+
+            modelBuilder.Entity<Video>()
+                .HasMany(p => p.Actores)
+                .WithMany(t => t.Videos)
+                .UsingEntity<VideoActor>(
+                    pt => pt.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+
+
+        }
+
+
+        public DbSet<Streamer>? Streamers { get; set; }
+
+        public DbSet<Video>? Videos { get; set; }
+
+        public DbSet<Actor>? Actores { get; set; }
+
+        public DbSet<Director>? Directores { get; set; }
+
+    }
+}
