7626311e7d2a27866a0a02518b7999f2a5183d0d
Kevin Camargo Bautista <cmargokk@hotmail.com>
2023-10-17 23:33:54 -0500

    Infrastructure dependency container and appsettings.json configuration

--- CleanArchitecture.Api/Program.cs
+++ CleanArchitecture.Api/Program.cs
@@ -1,9 +1,11 @@
 using CleanArchitecture.Application.Configuration;
+using CleanArchitecture.Infrastructure.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationServices();
+builder.Services.AddInfrastrctureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ CleanArchitecture.Data/Configuration/InfrastrctureServiceRegistration.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Application.Behaviours;
+using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Infrastructure.Email;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Configuration
+{
+    public static class InfrastrctureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastrctureServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<StreamerDbContext>(options 
+                => options.UseSqlServer(config.GetConnectionString("StreamerDB")));            
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IVideoRepository, VideoRepository>();
+            services.AddScoped<IStreamerRepository, StreamerRepository>();
+
+            services.Configure<EmailSettings>(c => config.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
