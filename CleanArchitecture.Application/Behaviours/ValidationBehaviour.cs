0acf1c111b19e0a5274bdac513cb4785fee7f433
Kevin Camargo Bautista <cmargokk@hotmail.com>
2023-10-17 19:13:02 -0500

    Dependency inversion container for applications layer

--- CleanArchitecture.Application/Behaviours/ValidationBehaviour.cs
+++ CleanArchitecture.Application/Behaviours/ValidationBehaviour.cs
@@ -4,7 +4,7 @@ using ValidationException = CleanArchitecture.Application.Exceptions.ValidationE
 
 namespace CleanArchitecture.Application.Behaviours
 {
-    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
         private readonly IEnumerable<IValidator<TRequest>> _validators;
         public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
