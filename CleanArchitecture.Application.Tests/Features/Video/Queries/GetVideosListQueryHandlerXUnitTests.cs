1a73717b925e997bb2090ae48613f8a0e22f4599
Kevin Camargo Bautista <cmargokk@hotmail.com>
2023-10-21 22:57:04 -0500

    unit test for GetVideosListQueryHandler

---
+++ CleanArchitecture.Application.Tests/Features/Video/Queries/GetVideosListQueryHandlerXUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.UnitOfWork;
+using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+using CleanArchitecture.Application.Tests.Mocks;
+using CleanArchitecture.Application.Utils.Mappings;
+using FluentAssertions;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Application.Tests.Features.Video.Queries
+{
+    public class GetVideosListQueryHandlerXUnitTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        public GetVideosListQueryHandlerXUnitTests()
+        {
+            _unitOfWork = MockUnitOfWork.GetUnitOfWork();
+            var mapperConfig = new MapperConfiguration(
+                c => c.AddProfile<MappingProfile>());
+
+            _mapper = mapperConfig.CreateMapper();           
+        }
+
+        [Fact]
+        public async Task Handle_whenIsCalled_shouldReturnVideosList() 
+        {
+            var handler = new GetVideosListQueryHandler(_mapper,_unitOfWork.Object);
+
+            var request = new GetVideosListQuery("systems");
+
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            result.ShouldBeOfType(typeof(List<VideoVm>));
+            result.ShouldBeOfType<List<VideoVm>>();
+
+            result.Should().BeOfType<List<VideoVm>>();
+            result.Should().BeOfType(typeof(List<VideoVm>));
+        }
+    }
+}
---
+++ CleanArchitecture.Application.Tests/Mocks/MockUnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Contracts.UnitOfWork;
+using CleanArchitecture.Domain;
+using Moq;
+
+namespace CleanArchitecture.Application.Tests.Mocks
+{
+    public class MockUnitOfWork
+    {
+        public static Mock<IUnitOfWork> GetUnitOfWork()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var mockVideoRepository = MockVideoRepository.GetVideoRepository();
+
+            mockUnitOfWork.Setup(r => r.VideoRepository).Returns(mockVideoRepository.Object);
+
+            return mockUnitOfWork;
+
+        }
+    }
+
+
+    public static class MockVideoRepository
+    {
+        public static Mock<IVideoRepository> GetVideoRepository()
+        {
+            var fixture = new Fixture();
+            //solucionamos el problema de referencias circular
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            var videos = fixture.CreateMany<Video>().ToList();
+
+            var mockRepository = new Mock<IVideoRepository>();
+
+            mockRepository.Setup( r => r.GetAllAsync()).ReturnsAsync(videos);
+
+            return mockRepository;
+        }
+    }
+}
